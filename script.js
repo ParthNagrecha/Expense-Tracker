017b017aab5e3c40bf15816890867bb2d33b9f89
Parth Nagrecha <parthnagrecha37@gmail.com>
2024-04-09 16:28:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let expinput = document.getElementById("expinput");
+let amtinput = document.getElementById("amtinput");
+let date = document.getElementById("dateinput");
+let addexp = document.getElementById('addexp');
+let del = document.getElementById('remove');
+
+addexp.addEventListener("click",function(){
+
+
+    var exp = expinput.value;
+    var amt = amtinput.value;
+    var dt = date.value;
+    
+    var tr = document.createElement('tr');
+
+    var td1 = tr.appendChild(document.createElement('td'));
+    var td2 = tr.appendChild(document.createElement('td'));
+    var td3 = tr.appendChild(document.createElement('td'));
+
+    td1.innerHTML = exp;
+    td2.innerHTML = amt;
+    td3.innerHTML = dt;
+
+    document.getElementById('tbl').appendChild(tr);
+
+    
+    del.addEventListener('click',function(){
+        document.getElementById('tbl').removeChild(tr);
+        })
+})
